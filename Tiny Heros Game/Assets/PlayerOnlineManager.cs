d9122d6b0976366a3840f3348a936a40be014a2f
CoderRamy <54765090+CoderRamy@users.noreply.github.com>
2020-11-29 00:59:21 +0200

    setup online mode

---
+++ Tiny Heros Game/Assets/PlayerOnlineManager.cs
@@ -0,0 +1,26 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOnlineManager : MonoBehaviourPunCallbacks
+{
+    public static GameObject LocalPlayerInstance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (photonView.IsMine)
+        {
+            LocalPlayerInstance = gameObject;
+            GetComponent<WalkingOrc>().enabled = true;
+        }
+        else
+        {
+            GetComponent<WalkingOrc>().enabled = false;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
