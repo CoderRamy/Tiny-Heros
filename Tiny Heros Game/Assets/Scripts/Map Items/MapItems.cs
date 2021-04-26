58064b14ca0f26b7ce00fd70a491eb005e2767c6
CoderRamy <54765090+CoderRamy@users.noreply.github.com>
2021-04-26 05:25:05 +0200

    Map Item System

---
+++ Tiny Heros Game/Assets/Scripts/Map Items/MapItems.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapItems : MonoBehaviour
+{
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Player hit the item");
+            MapRandomItems[] mapRandomItems = GameObject.Find("RandomObjects").GetComponents<MapRandomItems>();
+
+            foreach(var ItemScript in mapRandomItems)
+            {
+                if (ItemScript.MapItem.tag == gameObject.tag)
+                {
+                    Debug.Log("Destroy the item 1" + ItemScript.MapItem.name+" / "+ gameObject.name);
+                    ItemScript.Current_itemCount--;
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+    }
+
+}
+
