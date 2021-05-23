2c638036277a651481d01e86da88e603423dc6ee
CoderRamy <54765090+CoderRamy@users.noreply.github.com>
2021-05-23 09:38:20 +0200

    character select

---
+++ Tiny Heros Game/Assets/UICharacterSelect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICharacterSelect : MonoBehaviour
+{
+
+    public GameObject CharacterHolder;
+    public GameObject CharacterSelectorUI;
+    public GameObject MenuUI;
+
+    public void OpenSelector()
+    {
+        CharacterSelectorUI.SetActive(true);
+        MenuUI.SetActive(false);
+        CharacterHolder.transform.localScale = new Vector3(1.7f,1.7f,1.7f);
+    }
+    public void CloseSelector()
+    {
+        CharacterSelectorUI.SetActive(false);
+        MenuUI.SetActive(true);
+        CharacterHolder.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+}
