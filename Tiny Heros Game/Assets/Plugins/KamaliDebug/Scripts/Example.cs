c6370e740108bb45720279e2a0df4ae315ff766a
CoderRamy <54765090+CoderRamy@users.noreply.github.com>
2021-06-04 11:37:12 +0200

    fix marge after file organization

---
+++ Tiny Heros Game/Assets/Plugins/KamaliDebug/Scripts/Example.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using KamaliDebug;
+public class Example : MonoBehaviour
+{
+    private void Start()
+    {
+ //       DebugX.Log(@"I:orange:b; love:red:b; Unity:yellow:b;
+ //ColorfullllllllllllllllText:rainbow:b;");
+
+ //       DebugX.Log(@"sniper:orange:b; bug:green:b;");
+
+        DebugX.Log(@"Hiii Abdo:red;");
+
+    }
+}
