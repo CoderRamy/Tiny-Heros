278b4c3931304153772d8edf21f9b6887f2d8f5b
CoderRamy <54765090+CoderRamy@users.noreply.github.com>
2021-06-17 09:59:48 +0200

    chest system V0.1

---
+++ Tiny Heros Game/Assets/ChestLoot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestLoot : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] SpwnerAreas;
+
+    [SerializeField]
+    GameObject[] Rewards;
+
+    [SerializeField]
+    Animator anim;
+
+    // Salah W Osraa
+    public void Start()
+    {
+        StartOpenChest();
+    }
+
+    // Update is called once per frame
+    void StartOpenChest()
+    {
+        anim.SetBool("Open", true);
+    }
+
+    void OpenChest()
+    {
+
+        foreach (var area in SpwnerAreas)
+        {
+            GameObject item = Instantiate(Rewards[Random.Range(0, Rewards.Length)]);
+            item.transform.position = SpwnerAreas[Random.Range(0, SpwnerAreas.Length)].transform.position;
+        }
+
+          Destroy(gameObject);
+    }
+
+
+}
